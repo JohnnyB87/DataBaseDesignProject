1697a780c5d6702e6b063c24df1d0e3a78d5dd3b
JohnnyB87 <johnbrady1987@gmail.com>
2018-04-29 20:25:35 +0100

    started the add menu input

--- src/controllers/BranchWindow.java
+++ src/controllers/BranchWindow.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import classes.Main;
 import classes.PaneFrame;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
 
 public class BranchWindow {
 
@@ -20,23 +23,48 @@ public class BranchWindow {
     private TextField cityTxtFld;
     @FXML
     private TextField contactTxtFld;
-    private String name;
-    private String contactNo;
+    private String street;
+    private String city;
+    private String county;
+    private int contactNo;
+    private Connection con;
     private Button confirmButton;
+    private boolean contin = true;
 
     @FXML
     private void initialize(){
         if(this.paneFrame != null) {
             this.confirmButton = paneFrame.getConfirmButton();
-            this.confirmButton.setOnAction(e -> testButton());
+            confirmButton.setOnAction(e -> confirmButtonPressed());
         }
+        con = Main.getCon();
     }
 
-    private void testButton(){
-        System.out.println("ACCOUNT CONFIRM BUTTON");
+    private void confirmButtonPressed(){
+        this.contin = true;
+        this.street = validateTextFieldInputString(this.streetTxtFld.getText());
+        this.city = validateTextFieldInputString(this.cityTxtFld.getText());
+        this.county = validateTextFieldInputString(this.countyTxtFld.getText());
+        this.contactNo = validateTextFieldInputInt(this.contactTxtFld.getText());
+
+        if(!this.contin)
+            System.out.println("Enter correct input");
     }
 
     public Label getTitleLabel() {
         return titleLabel;
+    }
+
+    public String validateTextFieldInputString(String s){
+        if(s.matches("[a-zA-Z ]+"))
+            return s;
+        this.contin = false;
+        return null;
+    }
+    public int validateTextFieldInputInt(String s){
+        if(s.matches("[0-9]+"))
+            return Integer.parseInt(s);
+        this.contin = false;
+        return 0;
     }
 }
